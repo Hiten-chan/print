84df88a71c69d7f6313b41d469310b970c4774fb
macintosh <hiten-hemyra@mail.ru>
2018-03-27 20:40:46 +0300

    ok

--- intropage.php
+++ intropage.php
@@ -3,7 +3,7 @@
 session_start();
 
 if(!isset($_SESSION["session_username"])):
-    header("location:login.php");
+    header("location:login_o.php");
 else:
     ?>
 
---
+++ login_o.php
@@ -0,0 +1,69 @@
+<?php session_start();?>
+<?php require_once("includes/connection.php"); ?>
+<?php include("includes/header.php"); ?>
+<?php
+
+if (isset($_SESSION["session_username"])) {
+    header("Location: intropage.php");
+} else {
+}
+
+$message = '';
+$dbusername = '';
+$dbpassword = '';
+
+
+if (isset($_POST["login"])) {
+
+    $username = htmlspecialchars($_POST['username']);
+    $password = $_POST['password'];
+
+    if (!empty($username) && !empty($password)) {
+
+        $query = mysqli_query($link, "SELECT * FROM users WHERE username = '" . $username . "'");
+        $numrows = mysqli_num_rows($query);
+
+        if ($numrows != 0) {
+            while ($row = mysqli_fetch_assoc($query)) {
+
+                $dbusername = $row['username'];
+                $dbpassword = $row['password'];
+
+            }
+            if ($username == $dbusername && password_verify($password, $dbpassword)) {
+
+                $_SESSION['session_username'] = $username;
+                header('Location: intropage.php');
+                exit();
+
+            }
+        } else {
+            $message = "Неверное имя пользователя или пароль!";
+
+        }
+    } else {
+        $message = "Все поля обязательны для заполнения!";
+    }
+}
+?>
+
+
+    <body>
+    <div class="container mlogin">
+        <div id="login">
+            <h1>Вход</h1>
+            <span style="color:red"><?php echo $message; ?></span>
+            <form action="" id="loginform" method="post" name="loginform">
+                <p><label for="user_login">Имя пользователя<br>
+                        <input class="input" id="username" name="username" size="20"
+                               type="text" value=""></label></p>
+                <p><label for="user_pass">Пароль<br>
+                        <input class="input" id="password" name="password" size="20"
+                               type="password" value=""></label></p>
+                <p class="submit"><input class="button" name="login" type="submit" value="Вход"></p>
+                <p class="regtext">Еще не зарегистрированы? <br><a href="register.php">Регистрация!</a></p>
+            </form>
+        </div>
+    </div>
+    </body>
+<?php include("includes/footer.php"); ?>
