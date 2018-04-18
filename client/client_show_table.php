e27582226ba8ee620ee566848cd9e5b56491a58b
macintosh <hiten-hemyra@mail.ru>
2018-04-18 06:10:47 +0300

    add sorting function for client

--- client/client_show_table.php
+++ client/client_show_table.php
@@ -18,12 +18,12 @@ $total_rows = mysqli_num_rows($result);
 
 $colnames = explode(',', $colnames);
 
-$ruscolnames = '№ Заказа,Тип,Бумага,Размер,Кол-во,Дата создания,Срок исполнения,URL,Цена,Типограф,Статус';
+$ruscolnames = '№ Заказа,Тип,Бумага,Размер,Кол-во,Дата создания,Срок исполнения,URL,Цена (₽),Типограф,Статус';
 $ruscolnames = explode(',', $ruscolnames);
 
 
 if (!$total_rows) {
-    $structure .= "<HTML><BODY><h2>У вас не создано ни одного заказа</h2></BODY></HTML>" . "\r\n";
+    $structure .= "<HTML><BODY><h2>У вас не создано ни одного заказа</h2></BODY></HTML>\r\n";
     return;
 }
 
@@ -39,55 +39,58 @@ $orderid = '';
 $width_title = $total_cols + 1;
 
 //Формируем название таблицы
-$structure .= "<table width='100%' border='2' cellspacing='1' cellpadding='2' align='center' style='table-layout: auto; overflow: scroll'>" . "\r\n";
+$structure .= "<table id='table' class='sortable' width='100%' border='2' cellspacing='1' cellpadding='2' align='center' style='table-layout: auto; overflow: scroll'>\r\n";
 //$structure .= "<tr><td colspan=$width_title align=center style='font-weight: bold'>Ваши заказы</td></tr>" . "\r\n";
 
 
 //Формируем шапку таблицы
-$structure .= "<tr>" . "\r\n";
+$structure .= "<thead style='font-weight: 600'>\r\n<tr>\r\n";
+
 $i = 0;
 while ($i < count($ruscolnames)) {
-    $structure .= "<td align='center' style='background-color: #ffdeae'>" . $ruscolnames[$i] . "</td>" . "\r\n";
+// style='background-color: #ffdeae'
+    $structure .= "<td align='center'>$ruscolnames[$i]</td>\r\n";
     $i++;
 }
-$structure .= "<td style='background-color: #ffdeae'></td>" . "\r\n";
-$structure .= "</tr>" . "\r\n";
+$structure .= "<td class='nosort'></td>\r\n";
+$structure .= "</tr>\r\n</thead>\r\n";
 
 
-$structure .= "<tr>" . "\r\n";
+$structure .= "<tbody>\r\n<tr>\r\n";
 $i = 0;
 
 while ($i < $total_cols) {
     if ($i == $urlindex) {
-        $structure .= "<td align='center'>" . '<a href=' . $row[$i] . '>URL</a></td>' . "\r\n";
+        $structure .= "<td align='center'><a href=$row[$i]>URL</a></td>\r\n";
     } else if ($i == $statusindex) {
         if ($row[$i] == 'Отменен' || $row[$i] == 'Закрыт') {
-            $structure .= "<td align='center' style='color: rgba(255,0,4,0.93); font-weight: bolder'>" . $row[$i] . "</td>" . "\r\n";
+            $structure .= "<td align='center' style='color: rgba(255,0,4,0.93); font-weight: bolder'>$row[$i]</td>\r\n";
         } elseif ($row[$i] == 'В обработке' || $row[$i] == 'Подтвержден') {
-            $structure .= "<td align='center' style='color: #ffaa45; font-weight: bold'>" . $row[$i] . "</td>" . "\r\n";
+            $structure .= "<td align='center' style='color: #ffaa45; font-weight: bold'>$row[$i]</td>\r\n";
         } elseif ($row[$i] == 'Исполняется' || $row[$i] == 'Готов к выдаче') {
-            $structure .= "<td align='center' style='color: rgba(9,191,26,0.93); font-weight: bolder'>" . $row[$i] . "</td>" . "\r\n";
+            $structure .= "<td align='center' style='color: rgba(9,191,26,0.93); font-weight: bolder'>$row[$i]</td>\r\n";
         } else {
-            $structure .= "<td align='center'>" . $row[$i] . "</td>" . "\r\n";
+            $structure .= "<td align='center'>$row[$i]</td>\r\n";
         }
         $dbstatus = $row[$i];
 
     } else if ($i == $costindex) {
-        $structure .= "<td align='center'>" . $row[$i] . '₽' . "</td>" . "\r\n";
+        $structure .= "<td align='center'>$row[$i]</td>\r\n";
     } else {
-        $structure .= "<td align='center'>" . $row[$i] . "</td>" . "\r\n";
+        $structure .= "<td align='center'>$row[$i]</td>\r\n";
     }
 
     if ($i == $dborderid) {
         $orderid = $row[$i];
     }
+
     $i++;
 }
 if ($dbstatus == 'В обработке' || $dbstatus == 'Подтвержден') {
-    $structure .= "<td align='center'><form method='post'><input type='text' name='idorder' value=$orderid hidden='hidden'><input formaction='orders_history.php' class='button' name='cancel' type='submit' value='Отменить'></form></td>" . "\r\n";
+    $structure .= "<td align='center'><form method='post'><input type='text' name='idorder' value=$orderid hidden='hidden'><input formaction='orders_history.php' class='button' name='cancel' type='submit' value='Отменить'></form></td>\r\n";
 }
-$structure .= "<td align='center'></td>" . "\r\n";
-$structure .= "</tr>" . "\r\n";
+
+$structure .= "</tr>\r\n";
 
 
 while ($row = mysqli_fetch_row($result)) {
@@ -97,23 +100,23 @@ while ($row = mysqli_fetch_row($result)) {
     while ($i < $total_cols) {
 
         if ($i == $urlindex) {
-            $structure .= "<td align='center'>" . '<a href=' . $row[$i] . '>URL</a></td>' . "\r\n";
+            $structure .= "<td align='center'><a href=$row[$i]>URL</a></td>\r\n";
         } else if ($i == $statusindex) {
             if ($row[$i] == 'Отменен' || $row[$i] == 'Закрыт') {
-                $structure .= "<td align='center' style='color: rgba(255,0,4,0.93); font-weight: bolder'>" . $row[$i] . "</td>" . "\r\n";
+                $structure .= "<td align='center' style='color: rgba(255,0,4,0.93); font-weight: bolder'>$row[$i]</td>\r\n";
             } elseif ($row[$i] == 'В обработке' || $row[$i] == 'Подтвержден') {
-                $structure .= "<td align='center' style='color: #ffaa45; font-weight: bold'>" . $row[$i] . "</td>" . "\r\n";
+                $structure .= "<td align='center' style='color: #ffaa45; font-weight: bold'>$row[$i]</td>\r\n";
             } elseif ($row[$i] == 'Исполняется' || $row[$i] == 'Готов к выдаче') {
-                $structure .= "<td align='center' style='color: rgba(9,191,26,0.93); font-weight: bolder'>" . $row[$i] . "</td>" . "\r\n";
+                $structure .= "<td align='center' style='color: rgba(9,191,26,0.93); font-weight: bolder'>$row[$i]</td>\r\n";
             } else {
-                $structure .= "<td align='center'>" . $row[$i] . "</td>" . "\r\n";
+                $structure .= "<td align='center'>$row[$i]</td>\r\n";
             }
             $dbstatus = $row[$i];
 
         } else if ($i == $costindex) {
-            $structure .= "<td align='center'>" . $row[$i] . '₽' . "</td>" . "\r\n";
+            $structure .= "<td align='center'>$row[$i]</td>\r\n";
         } else {
-            $structure .= "<td align='center'>" . $row[$i] . "</td>" . "\r\n";
+            $structure .= "<td align='center'>$row[$i]</td>\r\n";
         }
 
         if ($i == $dborderid) {
@@ -123,10 +126,11 @@ while ($row = mysqli_fetch_row($result)) {
         $i++;
     }
     if ($dbstatus == 'В обработке' || $dbstatus == 'Подтвержден') {
-        $structure .= "<td align='center'><form method='post'><input type='text' name='idorder' value=$orderid hidden='hidden'><input formaction='orders_history.php' class='button' name='cancel' type='submit' value='Отменить'></form></td>" . "\r\n";
+        $structure .= "<td align='center'><form method='post'><input type='text' name='idorder' value=$orderid hidden='hidden'><input formaction='orders_history.php' class='button' name='cancel' type='submit' value='Отменить'></form></td>\r\n";
     } else {
-        $structure .= "<td align='center'></td>" . "\r\n";
+        $structure .= "<td align='center'></td>\r\n";
     }
-    $structure .= "</tr>" . "\r\n";
+    $structure .= "</tr>\r\n";
 }
+$structure .= "</tbody>\r\n";
 
--- client/orders_history.php
+++ client/orders_history.php
@@ -16,10 +16,11 @@ $tablename = 'orders';
 
 
 <?php include("../includes/header_account.php"); ?>
+<body>
 <div id="ohistory" class="content" style="display: block">
-    <div class="container_order_history">
+    <div class="container_order_history" style="overflow: scroll">
         <center>
-            <div id="settings">
+            <div id="settings" style="overflow: scroll">
                 <h1>Ваша история заказов</h1>
 
                 <?php include("client_show_table.php");
@@ -37,7 +38,46 @@ $tablename = 'orders';
                 }
                 ?>
                 <?php echo $structure; ?>
+
+
+                <div id="controls">
+                    <div class="perpage" id="perpage">
+                        <select id="perpage_select" onchange="sorter.size(this.value)">
+                            <option value="5">5</option>
+                            <option value="10" selected="selected">10</option>
+                            <option value="20">20</option>
+                            <option value="50">50</option>
+                            <option value="100">100</option>
+                        </select>
+<!--                        <span>Количество записей на странице</span>-->
+                    </div>
+                    <div id="navigation">
+                        <img src="../images/first.gif" width="16" height="16" alt="First Page" onclick="sorter.move(-1,true)" />
+                        <img src="../images/previous.gif" width="16" height="16" alt="First Page" onclick="sorter.move(-1)" />
+                        <img src="../images/next.gif" width="16" height="16" alt="First Page" onclick="sorter.move(1)" />
+                        <img src="../images/last.gif" width="16" height="16" alt="Last Page" onclick="sorter.move(1,true)" />
+                    </div>
+                    <div id="text">Страница <span id="currentpage"></span> из <span id="pagelimit"></span></div>
+                </div>
+                <script type="text/javascript" src="../scripts/diff_display.js"></script>
+                <script type="text/javascript">
+                    var sorter = new TINY.table.sorter("sorter");
+                    sorter.head = "head";
+                    sorter.asc = "asc";
+                    sorter.desc = "desc";
+                    sorter.even = "evenrow";
+                    sorter.odd = "oddrow";
+                    sorter.evensel = "evenselected";
+                    sorter.oddsel = "oddselected";
+                    sorter.paginate = true;
+                    sorter.currentid = "currentpage";
+                    sorter.limitid = "pagelimit";
+                    sorter.init("table",6);
+                </script>
+
+
             </div>
         </center>
     </div>
 </div>
+</body>
