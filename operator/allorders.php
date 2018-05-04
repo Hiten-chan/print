dc95d1f8f74d959b7b979c88aed4af5fc5e87de6
macintosh <hiten-hemyra@mail.ru>
2018-05-04 23:12:24 +0300

    change tool for order`s status changing

--- operator/allorders.php
+++ operator/allorders.php
@@ -20,28 +20,45 @@ $_SESSION['session_opage'] = 1;
         <center>
             <div id="settings">
                 <h1>Полная таблица заказов</h1>
-                <?php include("allorders_table.php");?>
+                <?php include("allorders_table.php"); ?>
+
+
+                <table border="0" align='center' style='table-layout: auto; overflow: scroll'>
+                    <tr>
+                        <td width="33%" style="padding: 0% 0% 0% 1%">
+                            <div class="perpage" id="perpage">
+                                <select id="perpage_select" onchange="sorter.size(this.value)">
+                                    <option value="5">5</option>
+                                    <option value="10" selected="selected">10</option>
+                                    <option value="20">20</option>
+                                    <option value="50">50</option>
+                                    <option value="100">100</option>
+                                </select>
+                            </div>
+                        </td>
+
+                        <td width="33%" style="vertical-align: top; padding: 0% 0% 0% 1%">
+                            <div id="navigation">
+                                <img src="../images/first.png" width="16" height="16" alt="First Page"
+                                     onclick="sorter.move(-1,true)"/>
+                                <img src="../images/previous.png" width="16" height="16" alt="First Page"
+                                     onclick="sorter.move(-1)"/>
+                                <img src="../images/next.png" width="16" height="16" alt="First Page"
+                                     onclick="sorter.move(1)"/>
+                                <img src="../images/last.png" width="16" height="16" alt="Last Page"
+                                     onclick="sorter.move(1,true)"/>
+                            </div>
+                        </td>
+
+                        <td width="33%" align="center" style="vertical-align: top; font-size: large">
+                            <div id="text">Страница <span id="currentpage"></span> из <span id="pagelimit"></span></div>
+                        </td>
+
+                    </tr>
+                </table>
+
                 <?php echo $structure; ?>
 
-                <div id="controls">
-                    <div class="perpage" id="perpage">
-                        <select id="perpage_select" onchange="sorter.size(this.value)">
-                            <option value="5">5</option>
-                            <option value="10" selected="selected">10</option>
-                            <option value="20">20</option>
-                            <option value="50">50</option>
-                            <option value="100">100</option>
-                        </select>
-                        <!--                        <span>Количество записей на странице</span>-->
-                    </div>
-                    <div id="navigation">
-                        <img src="../images/first.png" width="16" height="16" alt="First Page" onclick="sorter.move(-1,true)" />
-                        <img src="../images/previous.png" width="16" height="16" alt="First Page" onclick="sorter.move(-1)" />
-                        <img src="../images/next.png" width="16" height="16" alt="First Page" onclick="sorter.move(1)" />
-                        <img src="../images/last.png" width="16" height="16" alt="Last Page" onclick="sorter.move(1,true)" />
-                    </div>
-                    <div id="text">Страница <span id="currentpage"></span> из <span id="pagelimit"></span></div>
-                </div>
 
                 <script type="text/javascript" src="../scripts/diff_display.js"></script>
                 <script type="text/javascript" src="../scripts/sort_odisplay.js"></script>
